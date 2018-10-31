1e6d6e8d30c7dbcb5d89d37d65dd8c76515cbf5d
Fan Chen <chenfa@amazon.com>
2018-10-31 16:38:49 -0700

    learning

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+// Create a new component. This component should produce some HTML
+const youtubeAPI = 'AIzaSyCUCMPVYpQgj9rvHxQdU_iErQWmom1Hh3g';
 
-import App from './components/app';
-import reducers from './reducers';
+const  App = () => {
+    return (
+        <div>
+            <SearchBar/>
+        </div>);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it on the Page (in the DOM)
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
