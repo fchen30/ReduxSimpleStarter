1ded5aeccafbae65efa7d7d66d6e1311e08fdc3b
Fan Chen <fchen30@asu.edu>
2018-11-01 01:23:57 -0700

    video list, props

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 // Create a new component. This component should produce some HTML
 const youtubeAPI = 'AIzaSyCUCMPVYpQgj9rvHxQdU_iErQWmom1Hh3g';
 
-const  App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>);
-
-    };
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {videos: []};
+        YTSearch({key: youtubeAPI, term: 'surfboards'}, (videos) => {
+           this.setState({videos});
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos = {this.state.videos} />
+            </div>
+        );
+    }
+}
 
 // Take this component's generated HTML and put it on the Page (in the DOM)
 
